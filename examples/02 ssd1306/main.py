2dbfc15bb1c1f1f496e79cf1d371b82015b4770e
Jacob Zheng <1453366421@qq.com>
2025-09-16 18:13:24 +0800

    feat: 屏幕显示示例，更改引脚

--- examples/02 ssd1306/main.py
+++ examples/02 ssd1306/main.py
@@ -1,7 +1,7 @@
 import ssd1306
 from machine import I2C, Pin
 
-i2c = I2C(scl=Pin(22), sda=Pin(21))
+i2c = I2C(scl=Pin(18), sda=Pin(19))
 display = ssd1306.SSD1306_I2C(128, 64, i2c)
 
 display.text("Hello Python!", 0, 0)
