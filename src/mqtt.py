6f9ada85f69fe7715e6c91cfdd2323554a5863af
zhengjiajun <1453366421@qq.com>
2025-09-21 22:42:51 +0800

    feat: 更新MQTT连接配置，添加用户名和密码支持

--- src/mqtt.py
+++ src/mqtt.py
@@ -1,17 +1,24 @@
 from umqtt.simple import MQTTClient  # type: ignore
 
-# === MQTT 配置 ===
+# MQTT 配置
 MQTT_BROKER = "112.124.69.152"  # 你的 Ubuntu 服务器 IP
-MQTT_PORT = 1883
+MQTT_PORT = 7983
 MQTT_CLIENT_ID = "esp32s3_client"  # 唯一客户端 ID
-# MQTT_TOPIC_REQUEST = b"esp32s3/request"  # 发送请求的主题
-# MQTT_TOPIC_RESPONSE = b"esp32s3/response"  # 接收回复的主题
+MQTT_USERNAME = "esp32"
+MQTT_PASSWORD = "Esp32@0920"
 
 
 # 连接 MQTT
 def connect():
     print("Connecting to MQTT broker...")
-    client = MQTTClient(MQTT_CLIENT_ID, MQTT_BROKER, port=MQTT_PORT)
+
+    client = MQTTClient(
+        MQTT_CLIENT_ID,
+        MQTT_BROKER,
+        port=MQTT_PORT,
+        user=MQTT_USERNAME,
+        password=MQTT_PASSWORD,
+    )
     client.connect()
     print("✅ MQTT Connection successful!")
     return client
